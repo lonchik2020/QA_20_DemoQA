173157858cb5bc3d0e61ddb87966f459d6a687ce
lonchik2020 <krasleo@gmail.com>
2023-10-13 01:52:58 +0300

    css practice

--- src/test/java/cssxpathlocators/Cssxpath.java
+++ src/test/java/cssxpathlocators/Cssxpath.java
@@ -1,2 +1,50 @@
-package cssxpathlocators;public class Cssxpath {
+package cssxpathlocators;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Cssxpath {
+    WebDriver driver;
+
+    @Test
+    public void textBox(){
+        driver = new ChromeDriver();//because we work with google chrome
+        driver.manage().window().maximize(); //to make the window full screen
+        driver.get("https://demoqa.com/text-box");
+        //WebElement inputName = driver.findElement(By.cssSelector("input[#userName]"));//by id
+        WebElement inputName = driver.findElement(By.cssSelector("input[id='userName']"));//by input with id
+        System.out.println(inputName.getAttribute("placeholder"));
+        WebElement textArea = driver.findElement(By.cssSelector("[placeholder='Current Address']"));//by attribute
+        System.out.println(textArea.getTagName());
+
+        driver.quit();
+    }
+
+    @Test
+    public  void checkBox(){
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://demoqa.com/checkbox");
+        WebElement svg = driver.findElement(By.cssSelector("span[class='rct-text']>label>span[class='rct-node-icon']>svg"));
+        System.out.println(svg.getAttribute("stroke"));
+
+
+        driver.quit();
+    }
+
+    @Test
+    public  void radioButton(){
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://demoqa.com/radio-button");
+        WebElement radioButton = driver.findElement(By.cssSelector("div[class='col-12 mt-4 col-md-6'] :nth-child(3) label"));
+        System.out.println(radioButton.getText());
+
+
+        driver.quit();
+    }
+
 }
